651aac6d30ee8450dec76d86b2e72c749da27f01
Joseph Ampadu <josephampadu549@gmail.com>
2022-07-12 12:04:52 +0000

    good one

---
+++ app/models/quote.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class Quote < ApplicationRecord
+  validates :name, presence: true
+
+  scope :ordered, -> { order(id: :desc) }
+
+  broadcasts_to ->(quote) { "quotes" }, inserts_by: :prepend
+end
